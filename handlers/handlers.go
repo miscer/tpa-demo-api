ac9609b80e7e6baed95e1224652f733732bc4a26
Michal Miškerník <michal@miskernik.sk>
2021-03-16 11:05:53 +0100

    Added comments

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -16,12 +16,12 @@ type Handlers struct {
 }
 
 type AuthURLInput struct {
-	RedirectURL string `json:"redirect_url"`
+	RedirectURL string `json:"redirect_url"` // Frontend URL where the third party will redirect to
 }
 
 type AuthURLOutput struct {
-	AuthorizationURL string `json:"authorization_url"`
-	State            string `json:"state"`
+	AuthorizationURL string `json:"authorization_url"` // Third party URL where the user can log in
+	State            string `json:"state"`             // Frontend has to save the state somewhere to do a CSRF check later
 }
 
 func (h *Handlers) GetAuthURL(c *gin.Context) {
@@ -32,9 +32,14 @@ func (h *Handlers) GetAuthURL(c *gin.Context) {
 		return
 	}
 
+	// Only Facebook is supported for the demo.
 	provider := h.createFacebookProvider(input.RedirectURL)
 
+	// Random nonce is generated for each authorization request.
 	state := generateStateToken()
+
+	// The session is created only to get the authorization URL in the next step, it is not saved anywhere. It does not
+	// contain anything useful at this point anyway.
 	session, err := provider.BeginAuth(state)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Authorization failed: %v", err)
@@ -63,18 +68,18 @@ func (p Params) Get(key string) string {
 }
 
 type CompleteAuthInput struct {
-	State       string `json:"state"`
-	RedirectURL string `json:"redirect_url"`
-	Params      Params `json:"params"`
+	State       string `json:"state"`        // State returned from the first request, stored in the browser local storage
+	RedirectURL string `json:"redirect_url"` // Same URL that was supplied to the first request
+	Params      Params `json:"params"`       // Query parameters from the third party, usually contains the code and state
 }
 
 type CompleteAuthSuccessOutput struct {
-	Email string `json:"email"`
+	Email string `json:"email"` // Email address retrieved from the third party
 }
 
 type CompleteAuthErrorOutput struct {
-	Error    string `json:"error"`
-	RetryURL string `json:"retry_url"`
+	Error    string `json:"error"`     // Error code that frontend uses to show a suitable error message
+	RetryURL string `json:"retry_url"` // If the error can be fixed, frontend can redirect the user to this URL
 }
 
 func (h *Handlers) CompleteAuth(c *gin.Context) {
@@ -85,6 +90,8 @@ func (h *Handlers) CompleteAuth(c *gin.Context) {
 		return
 	}
 
+	// State returned from the first request (which is stored in browser local storage) has to be the same as the state
+	// returned from the third party to prevent CSRF attacks.
 	if input.State != input.Params.Get("state") {
 		c.JSON(http.StatusOK, CompleteAuthErrorOutput{
 			Error: "state_token_mismatch",
@@ -92,8 +99,10 @@ func (h *Handlers) CompleteAuth(c *gin.Context) {
 		return
 	}
 
+	// The redirect URL has to be the same to make sure the provider has the same configuration.
 	provider := h.createFacebookProvider(input.RedirectURL)
 
+	// Forward any errors from the third party to frontend. In a proper implementation this should be more clever.
 	if input.Params.Get("error") != "" {
 		c.JSON(http.StatusOK, CompleteAuthErrorOutput{
 			Error: input.Params.Get("error"),
@@ -101,29 +110,38 @@ func (h *Handlers) CompleteAuth(c *gin.Context) {
 		return
 	}
 
+	// Recreate the session from the first request
 	session, err := provider.BeginAuth(input.State)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Authorization failed: %v", err)
 		return
 	}
 
+	// Get the access token from the query params returned from the third party. For OAuth only the `code` parameter
+	// is used to get the access token, in combination with the redirect URL and client ID and secret.
 	_, err = session.Authorize(provider, input.Params)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Authorization failed: %v", err)
 		return
 	}
 
+	// Get user info, authenticated by the access token
 	user, err := provider.FetchUser(session)
 	if err != nil {
 		c.String(http.StatusInternalServerError, "Fetching user failed: %v", err)
 		return
 	}
 
+	// For demo purposes only. Email can be missing for multiple reasons, here we assume that the user disabled the
+	// email permission on Facebook.
 	if user.Email == "" {
+		// We can ask the user for the permission by adding `?auth_type=rerequest` to the Facebook authorization URL.
+		// Instead of manipulating the URL directly, we can use a custom provider and tell the OAuth library to add it.
 		if p, ok := provider.(*facebook.Provider); ok {
 			p.SetOptions(facebook.RerequestOption)
 		}
 
+		// Create a new session to make sure a new authorization URL is created, with the `auth_type` parameter.
 		session, err := provider.BeginAuth(input.State)
 		if err != nil {
 			c.String(http.StatusInternalServerError, "Authorization failed: %v", err)
@@ -136,6 +154,7 @@ func (h *Handlers) CompleteAuth(c *gin.Context) {
 			return
 		}
 
+		// User can fix the error by following the new authorization URL.
 		c.JSON(http.StatusOK, CompleteAuthErrorOutput{
 			Error:    "email_missing",
 			RetryURL: url,
